b3dcaf61278798537ca397df169826559767194f
yoosunu <yoosunu010@naver.com>
2024-11-03 11:17:56 +0900

    domain changed

--- src/api.ts
+++ src/api.ts
@@ -8,7 +8,7 @@ const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:8000/api/v1/"
-      : "https://apot-xif6.onrender.com/api/v1/",
+      : "https://apot.pro/api/v1/",
   withCredentials: true,
 });
 
