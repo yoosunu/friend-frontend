f354168f67ca9eafbe373f0231124959e6f06e64
yoosunu <yoosunu010@naver.com>
2024-11-14 11:37:38 +0900

    getMe headet added

--- src/api.ts
+++ src/api.ts
@@ -49,8 +49,15 @@ export const signUp = ({ username, password, email }: ISignUpVars) =>
     .then((response) => response.data);
 
 // login
-export const getMe = () =>
-  instance.get(`users/me`).then((response) => response.data);
+export const getMe = () => {
+  const response = instance
+    .get(`users/me`, {
+      headers: {
+        "X-CSRFToken": Cookie.get("csrftoken") || "",
+      },
+    })
+    .then((response) => response.data);
+};
 
 export interface IGHLoginVars {
   code: string;
